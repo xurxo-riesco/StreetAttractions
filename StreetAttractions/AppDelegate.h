28f192d666226c817ae12deba5df308cd5413fbb
Xurxo Riesco <xurxor@fb.com>
2020-07-13 12:03:57 -0400

    Initial Commit

---
+++ StreetAttractions/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StreetAttractions
+//
+//  Created by Xurxo Riesco on 7/13/20.
+//  Copyright © 2020 Xurxo Riesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ StreetAttractions/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  StreetAttractions
+//
+//  Created by Xurxo Riesco on 7/13/20.
+//  Copyright © 2020 Xurxo Riesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+

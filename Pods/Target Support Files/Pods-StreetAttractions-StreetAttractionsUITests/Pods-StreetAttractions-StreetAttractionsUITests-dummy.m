268c53ec021e06c7a563be6c821f32e426a6182e
Xurxo Riesco <xurxor@fb.com>
2020-07-13 12:32:26 -0400

    Initial Commit

---
+++ Pods/Target Support Files/Pods-StreetAttractions-StreetAttractionsUITests/Pods-StreetAttractions-StreetAttractionsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StreetAttractions_StreetAttractionsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_StreetAttractions_StreetAttractionsUITests
+@end
---
+++ Pods/Target Support Files/Pods-StreetAttractions/Pods-StreetAttractions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StreetAttractions : NSObject
+@end
+@implementation PodsDummy_Pods_StreetAttractions
+@end
